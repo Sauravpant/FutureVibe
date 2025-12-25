8af28bba5fb83ac77dcba19f4c1b9a981b1155fb
Saurav Pant <sauravpant777@gmail.com>
2025-12-25 10:04:42 +0545

    feat: add user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  authProvider: "local" | "google";
+  refreshToken?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true, 
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    authProvider: {
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", userSchema);
