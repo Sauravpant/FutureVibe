bc747d94794c1b8b9dfd29965b97c2f3b5132e8a
Saurav Pant <sauravpant777@gmail.com>
2025-12-25 12:05:21 +0545

    feat: initialize FastAPI application

---
+++ ml-backend/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI(title="FutureVibe ML Backend")
+
+
+class PredictRequest(BaseModel):
+    user_id: int
+
+
+@app.get("/")
+def health_check():
+    return {"message": "FastAPI ML Backend is running.!"}
+
+
+
+@app.post("/predict")
+def predict(request: PredictRequest):
+    user_id = request.user_id
+
+    # Prediction logic
+
+    return {"prediction": "Your prediction result here"}
+
