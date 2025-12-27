f9f1ee8a2ca2e7b9eaf113f4b1d986a3d9ec8f6a
Saurav Pant <sauravpant777@gmail.com>
2025-12-27 07:29:54 +0545

    feat: add Prediction model for storing user predictions

---
+++ backend/src/models/prediction.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IPrediction {
+  user: Schema.Types.ObjectId;
+  predictionText: string;
+  isPublic: boolean;
+  viewsCount: number;
+  imageUrl: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+const predictionSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    predictionText: {
+      type: String,
+      required: true,
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    viewsCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Prediction = mongoose.model<IPrediction>("Prediction", predictionSchema);
